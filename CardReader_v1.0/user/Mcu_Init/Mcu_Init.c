74e25ad4c02318a678d191097c31bf0cc68ede01
liujian <531587156@qq.com>
2018-12-10 15:55:36 +0800

    优化Mcu_Init模块：优化不使用的GPIO引脚

--- CardReader_v1.0/user/Mcu_Init/Mcu_Init.c
+++ CardReader_v1.0/user/Mcu_Init/Mcu_Init.c
@@ -29,6 +29,7 @@ description： function declaration
 
 /*Static function declaration*/
 //static SeMcu_Init_BaudRate;
+static void Mcu_Init_GPIOConfig(void);
 
 /******************************************************
 description： function code
@@ -46,83 +47,83 @@ description： function code
 ******************************************************/
 void Mcu_Init(void)
 {
-	//uint8 Le_u_BaudRate[4U];
-	/*时钟初始化配置*/
+	/* 时钟初始化配置 */
 	CLK_DeInit();/*内部晶振初始化*/
 	CLK_HSICmd(ENABLE);/*启用或关闭内部高速振荡器（HSI）*/
 	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);/*16Mz*/
 	
-	/*uart初始化*/
+	/* GPIO初始化 */
+	Mcu_Init_GPIOConfig();
+	
+	/* uart初始化 */
 	UART1_DeInit();
 	UART1_Init((uint32_t)9600,UART1_WORDLENGTH_8D,UART1_STOPBITS_1,UART1_PARITY_NO,UART1_SYNCMODE_CLOCK_DISABLE,UART1_MODE_TXRX_ENABLE);
-        UART1->CR2 &= (uint8_t)(~0x80);/* 禁止发送寄存器空中断 */
+    UART1->CR2 &= (uint8_t)(~0x80);/* 禁止发送寄存器空中断 */
 	UART1->CR2 &= (uint8_t)(~UART1_CR2_TEN);/* 禁止发送 */
 	UART1->CR2 |= (uint8_t)0x20; /* 接收中断请求 */
 	UART1->CR2 |= (uint8_t)UART1_CR2_REN; /* 允许接收 */
 	UART1_Cmd(ENABLE);	/* Enable the UART1*/
-#if 0       
-	//UART1->BRR2 = 0x02;/*9600波特率*/
-	//UART1->BRR1 = 0x68;
-	if(1U == MemIf_ReadEE(EepromCfg_UartBaudrate,Le_u_BaudRate,4U))
-	{
-		Mcu_Init_SetBaudRtae(*((uint32 *)(&Le_u_BaudRate[0U])));
-	}
-	else
-	{
-		Mcu_Init_SetBaudRtae(9600);/*设置默认波特率9600bit/s*/
-	}
-    UART1->CR1 = 0x00; 
-    UART1->CR2 |= (uint8_t)(UART1_CR2_TEN | UART1_CR2_REN);  
-    UART1->CR3 = 0x00; 
-	UART1->CR2 &= (uint8_t)(~0x80);/* 禁止发送寄存器空中断 */
-	UART1->CR2 &= (uint8_t)(~UART1_CR2_TEN);/* 禁止发送 */
-	UART1->CR2 |= (uint8_t)0x20; /* 接收中断请求 */
-	UART1->CR2 |= (uint8_t)UART1_CR2_REN; /* 允许接收 */
-	UART1->CR1 &= (uint8_t)(~UART1_CR1_UARTD);/* Enable the UART1*/ 
-#endif
 
 	/* SPI初始化 */
 	SPI_DeInit();
 	/* Initialize SPI in MASTER mode  */
 	SPI_Init(SPI_FIRSTBIT_MSB, SPI_BAUDRATEPRESCALER_4, SPI_MODE_MASTER, SPI_CLOCKPOLARITY_HIGH,SPI_CLOCKPHASE_2EDGE, SPI_DATADIRECTION_2LINES_FULLDUPLEX, SPI_NSS_SOFT,(uint8_t)0x07);/*spi 4Mz*/
 	SPI_Cmd(ENABLE);
-	//SPI->CR1 = 0x4F;
-	///SPI->CR2 = 0x03;
-	//SPI->CRCPR = 0x07;
 	
-	
-	/*gpio口初始化*/
-	//GPIO_DeInit(GPIOC);
-	//GPIO_Init(GPIOC,GPIO_PIN_3,GPIO_MODE_OUT_PP_HIGH_FAST);
-	//GPIO_Init(GPIOC,GPIO_PIN_4,GPIO_MODE_OUT_PP_HIGH_FAST);
-	GPIOC->CR2 = 0x00;
-	GPIOC->ODR = 0x18;
-	GPIOC->DDR = 0x18;
-	GPIOC->CR1 = 0x18;
-	//GPIO_Init(GPIOD,GPIO_PIN_1,GPIO_MODE_OUT_PP_HIGH_FAST);
-    //GPIO_Init(GPIOD,GPIO_PIN_5,GPIO_MODE_OUT_PP_HIGH_FAST);
-	//GPIO_Init(GPIOD,GPIO_PIN_6,GPIO_MODE_IN_PU_NO_IT);		
-	GPIOD->CR2 = 0x00;
-	GPIOD->ODR = 0x22;
-	GPIOD->DDR = 0x22;
-	GPIOD->CR1 = 0x62;
-	
-	//CFG->GCR = 0x01;/*初始化SWD编程口为普通I/O口*/
-
-
-	//stm8s独立看门狗配置
+	/* stm8s独立看门狗配置 */
     IWDG_Enable();//使能看门狗 
     IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);//同意操作独立看门狗
     IWDG_SetPrescaler(IWDG_Prescaler_256);//看门狗预分频
     IWDG_SetReload(75);//设置看门狗复位时间300ms
     IWDG_ReloadCounter();//喂狗
        
-   	enableInterrupts();/*全局总中断*/     
+   	enableInterrupts();/*全局总中断*/
+}
+
+/*
+	GPIO 配置
+*/
+static void Mcu_Init_GPIOConfig(void)
+{
+	//GPIO_DeInit(GPIOA);
+	//GPIO_DeInit(GPIOB);
+	//GPIO_DeInit(GPIOC);
+	//GPIO_DeInit(GPIOD);
+	GPIO_Init(GPIOC,GPIO_PIN_3,GPIO_MODE_OUT_PP_HIGH_FAST);
+	GPIO_Init(GPIOC,GPIO_PIN_4,GPIO_MODE_OUT_PP_HIGH_FAST);
+	//GPIO_Init(GPIOC,GPIO_PIN_5,GPIO_MODE_OUT_PP_HIGH_FAST);//spi SCK
+	//GPIO_Init(GPIOC,GPIO_PIN_6,GPIO_MODE_OUT_PP_HIGH_FAST);//spi MOSI
+	//GPIO_Init(GPIOC,GPIO_PIN_7,GPIO_MODE_IN_FL_NO_IT);//spi MISO
 	
+	GPIO_Init(GPIOD,GPIO_PIN_5,GPIO_MODE_OUT_PP_HIGH_FAST);//uart Tx
+	GPIO_Init(GPIOD,GPIO_PIN_6,GPIO_MODE_IN_FL_NO_IT);//uart Rx
+	
+	//GPIOC->CR2 = 0x00;
+	//GPIOC->ODR = 0x18;
+	//GPIOC->DDR = 0x18;
+	//GPIOC->CR1 = 0x18;	
+	//GPIOD->CR2 = 0x00;
+	//GPIOD->ODR = 0x22;
+	//GPIOD->DDR = 0x22;
+	//GPIOD->CR1 = 0x62;
+	
+	/**************** 未使用引脚处理 ****************/
+	GPIO_Init(GPIOA,GPIO_PIN_1,GPIO_MODE_IN_PU_NO_IT);//PA1 设置为上拉输入
+	GPIO_Init(GPIOA,GPIO_PIN_2,GPIO_MODE_IN_PU_NO_IT);//PA2 设置为上拉输入
+	//GPIO_Init(GPIOA,GPIO_PIN_3,GPIO_MODE_IN_PU_NO_IT);//PA3 设置为上拉输入
+	
+	GPIO_Init(GPIOB,GPIO_PIN_4,GPIO_MODE_IN_PU_NO_IT);//PB4 设置为上拉输入
+	GPIO_Init(GPIOB,GPIO_PIN_5,GPIO_MODE_IN_PU_NO_IT);//PB5 设置为上拉输入
+	
+	CFG->GCR = 0x01;/*初始化SWD编程口为普通I/O口*/
+	GPIO_Init(GPIOD,GPIO_PIN_1,GPIO_MODE_IN_PU_NO_IT);//PD1 设置为上拉输入
+	/*********************************************/	
 }
 
 
-
+/*
+	设置波特率
+*/
 void Mcu_Init_SetBaudRtae(uint32 BaudRate)
 {
 	uint32 BaudRate_Mantissa = 0, BaudRate_Mantissa100 = 0;
